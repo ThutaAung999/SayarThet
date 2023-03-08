a49351d6761caf7a17a26c32cb24086246b04980
ThutaAung999 <exercise999123@gmail.com>
2023-03-09 00:16:59 +0630

    Before using ngx-bootstrap Modal instead div tag

--- frontend/src/app/form/reactive-form-demo/reactive-form-demo.component.ts
+++ frontend/src/app/form/reactive-form-demo/reactive-form-demo.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-reactive-form-demo',
@@ -8,7 +8,38 @@ import {FormControl} from "@angular/forms";
 })
 export class ReactiveFormDemoComponent {
 
-  name=new FormControl('')
-  year=new FormControl('')
+  movieForm;
+
+  /*movieForm=new FormGroup({
+    name : new FormControl('',
+                [Validators.required,Validators.minLength(4)]),
+    year : new FormControl(''),
+    director : new FormControl('')
+  });
+*/
+
+  constructor(private formBuilder : FormBuilder){
+    this.movieForm=this.formBuilder.group({
+      name :['', [Validators.required,Validators.minLength(4)]],
+      year:['',[Validators.required,  Validators.pattern("^[0-9]*$")]],
+      director :['',Validators.required]
+    });
+  }
+  formSubmit(){
+    console.log("Movie Name : ", this.movieForm.value);
+  }
+
+  updateName(){
+    /*this.name.setValue("Updated name");*/
+  }
+
+  keyPress(event: any) {
+    const pattern = /[0-9\+\-\ ]/;
+
+    let inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
 
 }
